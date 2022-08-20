75c4ef9b6019bc0e50be6ea6d3438f21c589f104
alon manshari <alonman17@gmail.com>
2022-08-20 17:03:31 +0300

    adde images, adjusted footer size

--- client/src/clientsrc/Pages/Students.jsx
+++ client/src/clientsrc/Pages/Students.jsx
@@ -1,39 +1,38 @@
-import { darken } from "@mui/material";
-import React from "react";
-import { Outlet } from "react-router";
-import daniel from '../../assets/daniel.jpeg'
-
-
+import { darken } from '@mui/material';
+import React from 'react';
+import { Outlet } from 'react-router';
+import daniel from '../../assets/daniel.jpeg';
+import alon from '../../assets/alon.jpg';
+import shon from '../../assets/shon.jpg';
+import rom from '../../assets/rom.jpg';
 
 const Students = () => {
   return (
     <main>
       <Outlet />
-      <div className="center_box">
-        <h1>
-          CAR BAZAR
-        </h1>
+      <div className='center_box'>
+        <h1>CAR BAZAR</h1>
       </div>
-      <div className="partners_containers ">
-        <div className="partners fade left">
+      <div className='partners_containers '>
+        <div className='partners fade left'>
           <b>Daniel Ohayon </b>
           <b>danielohayon420@gmail.com</b>
           <img id='student_image' src={daniel} alt='daniel' />
         </div>
-        <div className="partners fade left" style={{top:'40vh'}}>
+        <div className='partners fade left' style={{ top: '40vh' }}>
           <b>Rom Bardugo</b>
-          <b>danielohayon420@gmail.com</b>
-          <img id='student_image' src={daniel} alt='daniel' />
+          <b>rombardugo1@gmail.com</b>
+          <img id='student_image' src={rom} alt='daniel' />
         </div>
-        <div className="partners right fade">
+        <div className='partners right fade'>
           <b>Alon Manshari</b>
-          <b>danielohayon420@gmail.com</b>
-          <img id='student_image' src={daniel} alt='daniel' />
+          <b>alonman17@gmail.com</b>
+          <img id='student_image' src={alon} alt='daniel' />
         </div>
-          <div className="partners right fade" style={{top:'40vh'}}>
+        <div className='partners right fade' style={{ top: '40vh' }}>
           <b>Shon Hazan</b>
-          <b>danielohayon420@gmail.com</b>
-          <img id='student_image' src={daniel} alt='daniel' />
+          <b>shonhazan1@gmail.com</b>
+          <img id='student_image' src={shon} alt='daniel' />
         </div>
       </div>
     </main>
--- client/src/clientsrc/components/Footer.jsx
+++ client/src/clientsrc/components/Footer.jsx
@@ -1,88 +1,90 @@
-import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
-import Call from "./call/Call";
-import { useEffect,useState } from "react";
-import  axios  from "axios";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Call from './call/Call';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import { Button, CardActionArea, CardActions } from '@mui/material';
 
 const Footer = () => {
-
-const [data,setData] =useState({})
+  const [data, setData] = useState({});
 
   useEffect(() => {
-
-    axios.get('https://api.nomics.com/v1/currencies/ticker?key=f906d4ab452426968881801f8f91ed1aa1cbc6b7&ids=BTC,ETH,XRP,LTC')
-    .then(res=>{
-      console.log(res.data)
-      setData(res.data);
-      console.log(Object.prototype.toString.apply(res.data))
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-  }, [])
-  
+    axios
+      .get(
+        'https://api.nomics.com/v1/currencies/ticker?key=f906d4ab452426968881801f8f91ed1aa1cbc6b7&ids=BTC,ETH,XRP,LTC'
+      )
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+        console.log(Object.prototype.toString.apply(res.data));
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <Box
-      component="footer"
+      component='footer'
       sx={{
         py: 0,
         px: 0,
-        mt: "auto",
+        mt: 'auto',
         background: 'linear-gradient(to right bottom, #430089, white)',
-        display: "flex",
-        flexDirection:'column',
-        justifyContent: "center",
-        alignItems: "center",
-        fontFamily:'monospace'
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontFamily: 'monospace',
       }}
-      position="sticky"
+      position='sticky'
       bottom={0}
     >
-         <h2 style={{color:'white',fontWeight:'bold'}}>Crypto Currency</h2>
-     { data[0] &&
-     <div className="footer" > 
-
- 
-      {data.map((item,index) => (
-      <Card  key={index} sx={{ minWidth: 245 , maxHeight:220, margin:'5px'}}>
-      <CardMedia
-        component="img"
-        height="60"
-        src={item['logo_url']}
-        alt="green iguana"
-      />
-      <CardContent>
-        <div style={{display: "flex",marginLeft:'1vw'}}>
-        <Typography gutterBottom variant="h6" component="div">
-          {item['id']}
-        </Typography>
-        <Typography gutterBottom variant="h6" component="div" marginLeft='1vw' >
-          {parseFloat( item['price']).toFixed(3)}$
-        </Typography>
+      <h2 style={{ color: 'white', fontWeight: 'bold' }}>Crypto Currency</h2>
+      {data[0] && (
+        <div className='footer'>
+          {data.map((item, index) => (
+            <Card
+              key={index}
+              sx={{ minWidth: 200, maxHeight: 180, margin: '5px' }}
+            >
+              <CardMedia
+                component='img'
+                height='60'
+                src={item['logo_url']}
+                alt='green iguana'
+              />
+              <CardContent>
+                <div style={{ display: 'flex', marginLeft: '1vw' }}>
+                  <Typography gutterBottom variant='h6' component='div'>
+                    {item['id']}
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    variant='h6'
+                    component='div'
+                    marginLeft='1vw'
+                  >
+                    {parseFloat(item['price']).toFixed(3)}$
+                  </Typography>
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <Typography variant='body1' color='text.secondary'>
+                    <b>Date: </b>
+                    {item['price_date'].slice(0, 10)}
+                  </Typography>
+                  <Typography variant='body1' color='text.secondary'>
+                    <b>Time: </b> {item['price_timestamp'].slice(11, -1)}
+                  </Typography>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
         </div>
-        <div style={{display: "flex",flexDirection:'column'}}>
-        <Typography variant="body1" color="text.secondary">
-            <b>Date: </b>{item['price_date'].slice(0,10)}
-        </Typography>
-        <Typography variant="body1" color="text.secondary">
-        <b>Time: </b> {item['price_timestamp'].slice(11,-1)}
-        </Typography>
-        </div>
-      </CardContent>
-      </Card>
-      ))}
-     
-
-  </div>}
+      )}
       <Call />
     </Box>
   );
