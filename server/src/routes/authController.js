71743705654d4e20740f26a6fe397c5c97d4389f
mika-israeli <mika80666@gmail.com>
2023-01-11 18:00:24 +0200

    change

--- server/src/routes/authController.js
+++ server/src/routes/authController.js
@@ -5,37 +5,45 @@ const jwt = require("jsonwebtoken");
 const { userValidationSchema } = require("../validation/validationsSchema");
 const validateSchema = require("../validation/validation");
 const User = require("../models/user.model");
-router.post("/register", userValidationSchema, validateSchema, async (req, res) => {
-  //check if user already exists
-  const usernameExists = await userService.getUserByUsernameOrEmail(req.body.username, req.body.email);
-  if (usernameExists) {
-    if (usernameExists.username === req.body.username) {
-      res.status(400).send({ message: "username already exists" });
+router.post(
+  "/register",
+  userValidationSchema,
+  validateSchema,
+  async (req, res) => {
+    //check if user already exists
+    const usernameExists = await userService.getUserByUsernameOrEmail(
+      req.body.username,
+      req.body.email
+    );
+    if (usernameExists) {
+      if (usernameExists.username === req.body.username) {
+        res.status(400).send({ message: "username already exists" });
+        return;
+      }
+      if (usernameExists.email === req.body.email) {
+        res.status(400).send({ message: "email already exists" });
+        return;
+      }
       return;
     }
-    if (usernameExists.email === req.body.email) {
-      res.status(400).send({ message: "email already exists" });
-      return;
+    //create the user and hash the password and save it to the database
+    try {
+      const hashedpassword = await bcrypt.hash(req.body.password, 10);
+      const username = req.body.username;
+      const email = req.body.email;
+      const password = hashedpassword;
+      const newUser = {
+        username: username,
+        password: password,
+        email: email,
+      };
+      const result = await userService.addUser(newUser);
+      res.status(201).json(result);
+    } catch (error) {
+      res.json(error);
     }
-    return;
   }
-  //create the user and hash the password and save it to the database
-  try {
-    const hashedpassword = await bcrypt.hash(req.body.password, 10);
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = hashedpassword;
-    const newUser = {
-      username: username,
-      password: password,
-      email: email,
-    };
-    const result = await userService.addUser(newUser);
-    res.status(201).json(result);
-  } catch (error) {
-    res.json(error);
-  }
-});
+);
 
 //LOGIN
 router.post("/login", userValidationSchema, async (req, res) => {
@@ -49,7 +57,10 @@ router.post("/login", userValidationSchema, async (req, res) => {
     return res.status(400).send("Invalid password");
   }
   //create and assign a token
-  const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.ACCESS_TOKEN_SECRET);
+  const token = jwt.sign(
+    { id: user._id, isAdmin: user.isAdmin },
+    "dsadsadasdasdasddasdasdasdsad"
+  );
   res.header("auth-token", token).send(user.isAdmin);
 });
 
